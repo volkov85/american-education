ffc7dd99bb007a33c007bcc9ec29d9c7ce23a18b
Volkov85 <xplz2007@gmail.com>
2019-01-06 17:34:39 +0600

    Slider

--- js/up_script.js
+++ js/up_script.js
@@ -65,42 +65,58 @@ var link5 = document.querySelector(".city-button-n");
 var link6 = document.querySelector(".city-button-o");
 var link7 = document.querySelector(".city-button-w");
 var popup = document.querySelector(".call-form-popup");
+var Username = popup.querySelector("[name=Username]");
 var close = popup.querySelector(".popup-close");
 var form = popup.querySelector("form");
+var textfield = document.querySelector(".about-call-form-action-text");
 
   link1.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("popup-show");
+    Username.focus();
+    textfield.value="Хочу учиться в Miami Beach";
   });
 
   link2.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("popup-show");
+    Username.focus();
+    textfield.value="Хочу учиться в Boston";
   });
 
   link3.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("popup-show");
+    Username.focus();
+    textfield.value="Хочу учиться в Los Angeles";
   });
 
   link4.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("popup-show");
+    Username.focus();
+    textfield.value="Хочу учиться в Santa Barbara";
   });
 
   link5.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("popup-show");
+    Username.focus();
+    textfield.value="Хочу учиться в New York";
   });
 
   link6.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("popup-show");
+    Username.focus();
+    textfield.value="Хочу учиться в Orlando";
   });
 
   link7.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("popup-show");
+    Username.focus();
+    textfield.value="Хочу учиться в Washington";
   });
 
 
