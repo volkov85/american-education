cf14e72d17e32d18a57e37804883f2665fe8c483
volkov85 <xplz2007@gmail.com>
2019-02-12 15:10:54 +0600

    phpmailer done all

--- js/script_summer.js
+++ js/script_summer.js
@@ -28,3 +28,19 @@ var link21 = document.querySelector(".summer-button");
       }
     }
   });
+
+  $("form").submit(function() { //Change
+  var th = $(this);
+  $.ajax({
+    type: "POST",
+    url: "mail.php", //Change
+    data: th.serialize()
+  }).done(function() {
+    alert("Спасибо за обращение!");
+    setTimeout(function() {
+      // Done Functions
+      th.trigger("reset");
+    }, 1000);
+  });
+  return false;
+});
